251527b2e97dd6d0ea3c8b7ccfe4697f03701494
Florian Farke <florian.farke@gmail.com>
2022-09-13 20:12:05 +0200

    overhauled the layout of the website

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,16 @@
-import * as bootstrap from 'bootstrap';
-
+import {Offcanvas, Tooltip} from 'bootstrap';
 
 window.addEventListener('DOMContentLoaded', () => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav != null) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler') as HTMLButtonElement;
-    const responsiveNavItems = Array.from(document.querySelectorAll('#navbarResponsive .nav-link'));
-
-    responsiveNavItems.map(responsiveNavItem => {
-        responsiveNavItem.addEventListener('click', () => {
-            if (navbarToggler != null && window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
+    // Resize fix for side-nav
+    window.matchMedia('(min-width: 1200px)')
+        .addEventListener('change', () => {
+            const element = document.getElementById('side-canvas');
+            if (element != null) {
+                Offcanvas.getOrCreateInstance(element).hide();
             }
         });
-    });
 
     // Initialize tooltips
     Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-        .map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+        .map(tooltipTriggerEl => new Tooltip(tooltipTriggerEl));
 });
